878909719f8508d4a2c8f70492b9d14a8e2a0158
pablo_blancoc <pablo_blancoc@outlook.es>
2020-07-25 12:44:08 -0500

    code: projects page all done

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='/img')
+    title = models.CharField(max_length=100, default="default")
+    short = models.CharField(max_length=100, default="default")
+    description = models.TextField(default="default")
+    technology1 = models.CharField(max_length=20, default="default")
+    technology2 = models.CharField(max_length=20, default="default")
+    technology3 = models.CharField(max_length=20, default="default")
+    link_text = models.CharField(max_length=20, default='default')
+    link = models.URLField(default='default')
+    image = models.FilePathField(path='/img', default="default")
